f9e0af1f3092295ba350d5aa9ed5b444c50d1ac1
Jiahao Li <reg@ljh.me>
2018-05-18 18:01:38 -0400

    Separates custom_log into its own library

--- lib/custom_log/include/custom_log.h
+++ lib/custom_log/include/custom_log.h
@@ -2,13 +2,17 @@
 
 #include <string.h>
 
-#include "log.h"
+#include <nrf_log.h>
+#include <log.h>
 
 #include <SEGGER_RTT.h>
 
 #define LOG_INFO(...) __LOG(LOG_SRC_APP, LOG_LEVEL_INFO, __VA_ARGS__)
 #define LOG_ERROR(...) __LOG(LOG_SRC_APP, LOG_LEVEL_ERROR, __VA_ARGS__)
 #define LOG_INIT() __LOG_INIT(LOG_SRC_APP, LOG_LEVEL_INFO, log_callback_custom)
+
+#define LOG_READING_FLOAT(name, value)                                         \
+  LOG_INFO("READING " name " " NRF_LOG_FLOAT_MARKER, NRF_LOG_FLOAT(value))
 
 /* For some reason, this function, while not static, is not included in the RTT
  * header files. */
