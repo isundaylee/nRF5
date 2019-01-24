8c325a54631fd94ad5726bacd978fc638a556418
Jiahao Li <reg@ljh.me>
2019-01-24 12:16:05 -0500

    Reduces battery voltage display precision

--- console/display.py
+++ console/display.py
@@ -47,7 +47,7 @@ def format_battery(battery):
     if battery is None:
         return 'N/A'
     else:
-        return "%.5f V" % battery
+        return "%.3f V" % battery
 
 
 def format_ratio(a, b):
@@ -63,7 +63,7 @@ def render(nodes):
     for addr, data in nodes.items():
         result += (('-' * 110) + '\n')
 
-        result += ("%-15s | %-30s %9s %10s %10s %7s | %-3s | %-20s\n" % (
+        result += ("%-15s | %-30s %7s %10s %10s %7s | %-3s | %-20s\n" % (
             data['name'],
             format_faults(data),
             format_battery(data['battery']),
@@ -76,7 +76,7 @@ def render(nodes):
             format_last_seen(data)))
 
         for ttl in sorted(data['avg_rssi_by_ttl'].keys(), reverse=True):
-            result += ("%-15s | %-30s %9s %10s %10s %7s | %-3s | %-20s\n" %(
+            result += ("%-15s | %-30s %7s %10s %10s %7s | %-3s | %-20s\n" %(
                     '',
                     '',
                     '',
