3e0bf6c5ccc35b7fdf07648547f3b3679ee480a9
Jiahao Li <reg@ljh.me>
2019-01-23 22:07:43 -0500

    Adds REGOUT setting for gateway app

--- apps/gateway/src/main.c
+++ apps/gateway/src/main.c
@@ -828,9 +828,32 @@ static void start() {
   }
 }
 
+void init_regout() {
+#ifdef NRF52840_XXAA
+  if ((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) ==
+      (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos)) {
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+    }
+
+    NRF_UICR->REGOUT0 =
+        (NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
+        (UICR_REGOUT0_VOUT_3V0 << UICR_REGOUT0_VOUT_Pos);
+
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+    }
+
+    // System reset is needed to update UICR registers.
+    NVIC_SystemReset();
+  }
+#endif
+}
+
 int main(void) {
   DEBUG_PINS_INIT();
 
+  init_regout();
   init_leds();
   init_logging();
   init_mesh();
